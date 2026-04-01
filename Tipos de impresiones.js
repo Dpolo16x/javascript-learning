a57684ca9349ce91eca0676a9923109040a4d497
Dpolo16x <diegopolo236@gmail.com>
2026-04-01 15:21:13 -0500

    Add additional console log examples
    añadir un ejemplo de las posibilidades de uso de la función console.log para sumar cadenas.

--- Tipos de impresiones.js
+++ Tipos de impresiones.js
@@ -3,4 +3,5 @@ console.log('Hola mundo');// IMPRIMIR CADENAS DE TEXTO
 console.log('La identificacion del computador es: ', 12983173912);// Impresion de cadenas y numeros 
 console.log('%cESTE ES un texto de ejemplo', 'color:blue');// una impresion con estilo(CSS), podemos citar funciones css en el
 console.log('la suma de los numeros 1999 y 17 es: ', 1999+17);// impresion compuesta
+console.log('cadena1'+'cadena2')// suma de cadenas en una impresion 
 
