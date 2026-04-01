1c44bc126c8f2d453ff03bc1d9a23fac7dc106c2
Dpolo16x <diegopolo236@gmail.com>
2026-04-01 17:21:27 -0500

    Tipos de datos en Javascript
    ejemplos de tipos de datos en javascript

---
+++ tipos de datos.js
@@ -0,0 +1,29 @@
+// String
+let nombre = "Diego";
+
+// Number
+let edad = 20;
+
+// Boolean
+let activo = true;
+
+// Undefined
+let sinValor;
+
+// Null
+let vacio = null;
+
+// Object
+let persona = {
+  nombre: "Diego",
+  edad: 20
+};
+
+// Array
+let numeros = [1, 2, 3];
+
+// BigInt
+let grande = 12345678901234567890n;
+
+// Symbol
+let id = Symbol("id");
