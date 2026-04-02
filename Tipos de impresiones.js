6e8e873f88455b3109c6ac00ec940c2a69a94128
Dpolo16x <diegopolo236@gmail.com>
2026-04-01 19:00:08 -0500

    Add additional console log examples
    concatenar datos numéricos a cadenas

--- Tipos de impresiones.js
+++ Tipos de impresiones.js
@@ -4,4 +4,6 @@ console.log('La identificacion del computador es: ', 12983173912);// Impresion d
 console.log('%cESTE ES un texto de ejemplo', 'color:blue');// una impresion con estilo(CSS), podemos citar funciones css en el
 console.log('la suma de los numeros 1999 y 17 es: ', 1999+17);// impresion compuesta
 console.log('cadena1'+'cadena2')// suma de cadenas en una impresion 
-
+// se pueden sumar datos numericos a cadenas de texto:
+console.log('salio la nueva pelicula spiderman '+3)
+console.log('el año tiene: '+365+' dias')
