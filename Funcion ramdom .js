690f57882a2044cd42fcb08d77d924d66db0642a
Diego Polo <diegopolo236@gmail.com>
2026-04-24 22:44:18 -0500

    Funcion random
    Uso de la funcion integrada random para valores aleatorios

---
+++ Funcion ramdom .js
@@ -0,0 +1,16 @@
+/*
+Random: sirve para devolver un valor decimal entre :
+                0 y 0.99
+
+sintaxis:
+         variable=math.random();
+salida ejemplo:0.234567891 // numero random 
+
+si queremos mostrar 
+
+*/
+nrandom1=Math.ceil(Math.random()*10)
+
+console.log(nrandom1)   
+
+amogus=typeof(19>27)
